97f83b6395d954a9f59eb572360f9cfe9500e3b4
hakbari14 <hakbari@email.com>
2026-06-08 08:26:57 +0000

    log files

--- src/confidence/settings_0/truthfulqa/multiple_choices_generation_truthfulqa.py
+++ src/confidence/settings_0/truthfulqa/multiple_choices_generation_truthfulqa.py
@@ -11,7 +11,6 @@ class multiple_choices_generation(multiple_choices_generation):
     def get_dataset(self) -> truthfulqa_dataset:
         if self.dataset is None:
             config = dataset_config(self.modelname)
-            config.set_ratio_test_dataset_size(0.2)
             self.dataset = truthfulqa_dataset(config)
         return self.dataset
 
@@ -19,7 +18,7 @@ class multiple_choices_generation(multiple_choices_generation):
         return multiple_choices_inference_logger(log_file_name = f'src/confidence/settings_0/truthfulqa/run_{run_number}/multiple_choices_truthfulqa.csv')
 
 
-for run_number in range(1,6):
+for run_number in range(6,11):
     print(f'{'*' * 100}  TruthfulQA : Run Number {run_number}  {'*' * 100}')
     t = multiple_choices_generation(modelname='deepseek-ai/DeepSeek-R1-Distill-Qwen-7B')
     t.generate_response(run_number = run_number)
