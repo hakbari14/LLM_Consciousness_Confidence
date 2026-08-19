05c6ba31c3ebc19908b569716e0f8c1078ba189e
hakbari14 <hakbari@email.com>
2026-08-19 05:48:42 +0000

    bug fixed!

--- src/diffusion_decision_model/gsm8k/diffusion_decision_model_gsm8k.py
+++ src/diffusion_decision_model/gsm8k/diffusion_decision_model_gsm8k.py
@@ -25,5 +25,5 @@ class diffusion_decision_model_gsm8k(diffusion_decision_model):
         return diffusion_decision_model_logger(log_file_name = f'src/diffusion_decision_model/gsm8k/{self.modelname}/run_{run_number}/diffusion_decision_model_gsm8k.csv')
 
 
-t = diffusion_decision_model_gsm8k(modelname='Qwen/Qwen3-8B', number_of_evidence=20)
+t = diffusion_decision_model_gsm8k(modelname='Qwen/Qwen3-8B')
 t.run(from_run_number=1, to_run_number=2)
