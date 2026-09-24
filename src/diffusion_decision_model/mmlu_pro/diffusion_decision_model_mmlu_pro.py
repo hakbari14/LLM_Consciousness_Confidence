8c61bcfc5ace27877990b4fe155f18b2f28649bb
hakbari14 <hakbari@email.com>
2026-09-24 05:29:11 +0000

    deepseek-r1 logs

--- src/diffusion_decision_model/mmlu_pro/diffusion_decision_model_mmlu_pro.py
+++ src/diffusion_decision_model/mmlu_pro/diffusion_decision_model_mmlu_pro.py
@@ -36,6 +36,17 @@ class diffusion_decision_model_mmlu_pro(diffusion_decision_model):
 # t = diffusion_decision_model_mmlu_pro(modelname='deepseek-ai/DeepSeek-R1-Distill-Qwen-7B', number_of_evidence=20)
 # t.run(from_run_number=1, to_run_number=2)
 
+# for nv in [5, 10, 15, 25]:
+#     print(f"{'*' * 100}  Number Of Evidence {nv}  {'*' * 100}")
+#     t = diffusion_decision_model_mmlu_pro(modelname='deepseek-ai/DeepSeek-R1-Distill-Qwen-7B', number_of_evidence=nv)
+#     t.run(from_run_number=1, to_run_number=2)
+#     print(f"{'*' * 210}")
+
+for nv in [5, 10, 15]:
+    print(f"{'*' * 100}  Number Of Evidence {nv}  {'*' * 100}")
+    t = diffusion_decision_model_mmlu_pro(modelname='deepseek-ai/DeepSeek-R1-Distill-Qwen-7B', number_of_evidence=nv)
+    t.baseline_features_extractor(from_run_number=1, to_run_number=2)
+    print(f"{'*' * 210}")
 
 # for nv in [5, 10, 15, 20, 25]:
 #     print(f"{'*' * 100}  Number Of Evidence {nv}  {'*' * 100}")
